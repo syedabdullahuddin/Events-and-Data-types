6555c8e69d014a5eceb438556c4e1b9b8d1aa0b9
Syed Abdullah <160462766+syedabdullahuddin@users.noreply.github.com>
2025-04-12 02:47:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// function popup(message) {
+//     alert(message);
+// }
+// var a=prompt("Enter  your first name: ");
+// var b=prompt("Enter your last name: ");
+// var c='Hello! '+ a +" "+b;
+// alert(c);
+
+// var a = prompt("Enter your favourite mobile name: ");
+// var b = a.length;
+// alert("Length of string is: " + b);
+
+// var a = "Pakistani";
+// var b = a.indexOf("n");
+// alert("Index of 'n' in Pakistani is: " + b);
+
+// var a = "Hello World!";
+// var b = a.lastIndexOf("l");
+// alert("Index of 'l' in Hello World! is: " + b);
+
+// var a = "Pakistani";
+// var b = a.charAt(3);
+// alert("Character at index 3 in Pakistani is: " + b);
+
+// var a = "Hyderabad";
+// var b = a.replace("Hyder","Islam");
+// alert("City: " + b);
+
+// var a = "Ali and Sami are best friends. They play cricket and football together.";
+// var b = a.replace(/and/g, "&");
+// alert(b);
+
+// var a = "472";
+// var b = Number(a);
+// alert("Value: " + a);
+
+// var a = prompt("Enter any word: ");
+// var b = a.toUpperCase();
+// alert("Upper case: " + b);
