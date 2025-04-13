4e44d7961523aa4c407d0c52f989a18d5b625f48
Syed Abdullah <160462766+syedabdullahuddin@users.noreply.github.com>
2025-04-13 08:32:34 +0500

    Update app.js

--- app.js
+++ app.js
@@ -37,3 +37,66 @@
 // var a = prompt("Enter any word: ");
 // var b = a.toUpperCase();
 // alert("Upper case: " + b);
+
+
+// var a = prompt("Enter any word: ");
+// var b = a.split(" ");
+//  for (var i = 0; i < b.length; i++) {
+// var c = b[i];
+// b[i] = c.charAt(0).toUpperCase()+c.slice(1).toLowerCase();
+//     }
+// var d = b.join(" ");
+// alert("TitleCase: " + d);
+
+
+// var a=prompt("Enter any word: ");
+// var b =a.split(" ");
+// for(var i=0;i<b.length;i++){
+//     var c=b[i];
+//     b[i]=c.charAt(0).toUpperCase()+c.slice(1).toLowerCase();
+// }
+// var d=b.join(" ");
+// alert("TitleCase: "+d);
+
+// var a=3455555.4832;
+// var b=a.toString().replace(".","")
+// alert("String: "+b);
+
+
+// var a = "3444.444,5.5525.5555";
+// var b=a.replaceAll(".","").replaceAll(",","");
+// alert("String: "+b);
+
+// var a=prompt("Enter your username: ");
+// var b=a.split("");
+// var c=b.indexOf("@")
+// b.indexOf(".")
+// b.indexOf("!")
+// b.indexOf(",")
+// if(c!==-1){
+//     alert("Please enter a valid username.");
+// }
+// else{
+//     alert("Valid username.");
+// }
+// while (true) {
+//     // Prompt user to enter a username
+//     var username = prompt("Enter your username:");
+
+//     // Check if username contains any special characters [@, ., !, ,]
+//     if (
+//         username.includes("@") ||
+//         username.includes(".") ||
+//         username.includes("!") ||
+//         username.includes(",")
+//     ) {
+//         // If special characters found, show valid username message
+//         alert("Valid username!.");
+//         break; // Exit loop when valid username is found
+//     } else {
+//         // If special characters not found, show invalid username message
+//         alert("Invalid username! You must include at least one of these special characters: @, ., !, ,");
+//     }
+// }
+
+
